8fc3d49c9d3b4749a0ae469aa49ca38fe4fb8140
stephan <stephan.orgiazzi@gmail.com>
2018-01-28 22:42:51 +0100

    added automatic sprites

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,10 @@
-// npm install gulp gulp-watch gulp-postcss autoprefixer postcss-simple-vars postcss-nested postcss-import postcss-mixins browser-sync --save-dev
+// npm install gulp gulp-watch gulp-postcss autoprefixer postcss-simple-vars postcss-nested postcss-import postcss-mixins browser-sync gulp-svg-sprite gulp-rename del --save-dev
 
-const gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins'),
-browserSync = require('browser-sync').create();
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
 
-gulp.task('default', function() {
-  console.log("Hooray - you created a Gulp task.");
-});
 
-gulp.task('html', function() {
-  console.log("Imagine something useful being done to your HTML here.");
-});
-
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-    .on("error", function(errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit("end");
-    })
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function() {
-
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "app"
-    }
-  });
-
-  watch('./app/index.html', function() {
-    browserSync.reload();
-  });
-
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
-
-});
-
-gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-     .pipe(browserSync.reload({ stream: true}));
-});
+// Generating sprites:
+// in command line : gulp icons
+// Use in HTML: <span class="icon--name"></span>
