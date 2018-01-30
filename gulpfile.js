dfabea74cfb0b6873e5475cd45329c74ad1ea6c6
stephan <stephan.orgiazzi@gmail.com>
2018-01-30 14:00:56 +0100

    added burger menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-// npm install gulp gulp-watch gulp-postcss autoprefixer postcss-simple-vars postcss-nested postcss-import postcss-mixins postcss-hexrgba browser-sync gulp-svg-sprite gulp-rename del --save-dev
+// npm install gulp gulp-watch gulp-postcss autoprefixer postcss-simple-vars postcss-nested postcss-import postcss-mixins postcss-hexrgba browser-sync gulp-svg-sprite gulp-rename del webpack --save-dev
+// npm install babel-core babel-loader babel-preset-env --save-dev
+// npm install jquery --save
 
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
-
+require('./gulp/tasks/scripts');
 
 // Generating sprites:
 // in command line : gulp icons
